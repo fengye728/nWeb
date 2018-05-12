8b5a4502c12bcaff5a70b0869769e7b4bb6317b6
Peter <UNKNOWN>
2018-05-12 16:28:35 +0800

    OI Chart version 2.0: Add OI Change chart and OI change search

--- src/main/resources/static/internal/AdminContents/oiChart/oi-chart.js
+++ src/main/resources/static/internal/AdminContents/oiChart/oi-chart.js
@@ -10,7 +10,13 @@ angular.module(window.tsc.constants.DASHBOARD_APP).component('oiChart', {
         
 		ctrl.optionTypeCategory = ['C', 'P'];
 		ctrl.optionOIModel = {};
+		ctrl.optionOIChange = {
+				minOIChange : null,
+				maxOIChange : null
+		};
+		ctrl.optionOIChangeResult = [];
 		
+// ------------------- Controller Functions -----------------------------------------------		
 		ctrl.searchFake = function() {
 			data = {};
 			data.stockSymbol = 'SNAP';
@@ -22,20 +28,36 @@ angular.module(window.tsc.constants.DASHBOARD_APP).component('oiChart', {
 			data.endEventDay = 180510;
 			
 			getOptionOIBetween(data).success(function(response){
-				ctrl.drawChart(response);
+				ctrl.drawOIChart(response);
+				ctrl.drawOIChangeChart(response)
 			});
 		}
 		
-// ------------------- Functions -----------------------------------------------
-		ctrl.search = function() {
-			if(ctrl.checkForm()) {
+
+		ctrl.draw = function() {
+			if(ctrl.checkOptionForm()) {
+				// disable button
+				$('#drawBtn')[0].disabled = true;
+				$('#drawBtn')[0].textContent = 'Drawing';
 				// search oi
-				getOptionOIBetween(getFormatData(ctrl.optionOIModel)).success(function(response){
+				getOptionOIBetween(getFormatOptionData(ctrl.optionOIModel)).success(function(response){
 					if(response.length > 0) {
-						ctrl.drawChart(response);
+						// draw chart
+						ctrl.drawOIChart(response);
+						ctrl.drawOIChangeChart(response);
+						
 					} else {
-						toastr.info('No Open Interest');
+						toastr.info('No Open Interest', 'Draw OI Chart');
 					}
+					// enable button
+					$('#drawBtn')[0].disabled = false;
+					$('#drawBtn')[0].textContent = 'Draw';
+				}).error(function(response){
+					toastr.error(response, 'Server Error:');
+					
+					// enable button
+					$('#drawBtn')[0].disabled = false;
+					$('#drawBtn')[0].textContent = 'Draw';
 				});
 			} else {
 				// leak params
@@ -43,30 +65,102 @@ angular.module(window.tsc.constants.DASHBOARD_APP).component('oiChart', {
 			}
 		};
 		
-		
-		// Check and format form 
-		ctrl.checkForm = function() {
-			count = 0;
-			for(var i in ctrl.optionOIModel) {
-				count++;
+		ctrl.searchOIChange = function() {
+			if(ctrl.checkOptionForm() && ctrl.checkOIChangeForm()) {
+				// disable button
+				$('#searchBtn')[0].disabled = true;
+				$('#searchBtn')[0].textContent = 'Searching';
+				// search oi
+				getOptionOIBetween(getFormatOptionData(ctrl.optionOIModel)).success(function(response){
+					if(response.length > 0) {
+						// process oi change data
+						var data = []
+						for(var i = 1; i < response.length; i++) {
+							x_date = new Date(response[i].eventDay / 10000 + 2000, response[i].eventDay % 10000 / 100 - 1, response[i].eventDay % 100);
+							y_oi = response[i].openInterest - response[i - 1].openInterest;
+							
+							if(ctrl.optionOIChange.minOIChange != null && y_oi < ctrl.optionOIChange.minOIChange) {
+								continue;
+							}
+							if(ctrl.optionOIChange.maxOIChange != null && y_oi > ctrl.optionOIChange.maxOIChange) {
+								continue;
+							}
+							
+							data.push({
+								x : formatDate(x_date),
+								y : y_oi
+							});
+						}
+						ctrl.optionOIChangeResult = data;
+						// show modal
+						$('#oiChangeModal').modal('show');
+						
+					} else {
+						toastr.info('No Open Interest', 'Search OI Change');
+					}
+					// enable button
+					$('#searchBtn')[0].disabled = false;
+					$('#searchBtn')[0].textContent = 'Search';
+				}).error(function(response){
+					toastr.error(response, 'Server Error:');
+					// enable button
+					$('#searchBtn')[0].disabled = false;
+					$('#searchBtn')[0].textContent = 'Search';
+				});				
+				
 			}
-			if(count < 6) {
+		};
+
+		
+// -------------------- Other function ---------------------
+		// Check optionOIModel and toastr error message
+		ctrl.checkOptionForm = function() {
+			valueCount = 0;
+			for(var col in ctrl.optionOIModel) {
+				if(ctrl.optionOIModel[col] != null && ctrl.optionOIModel[col] !== '') {
+					valueCount++;
+				}
+			}
+			if(valueCount < 6) {
+				toastr.error('Lack option parameters!');
 				return false;
 			} else {
 				return true;
 			}
 		};
 		
-// -------------------- Chart function ---------------------
-		ctrl.drawChart = function(oiList) {
-			option = oiList[0].stockSymbol + oiList[0].strike + oiList[0].callPut + oiList[0].expiration
+		// Check optionOIChange and toastr error message
+		ctrl.checkOIChangeForm = function() {
+			valueCount = 0;
+			for(var col in ctrl.optionOIChange) {
+				if(ctrl.optionOIChange[col] != null && ctrl.optionOIChange[col] !== '') {
+					valueCount++;
+				}
+			}
+			if(valueCount == 2) {
+				if(ctrl.optionOIChange.minOIChange > ctrl.optionOIChange.maxOIChange) {
+					toastr.error('Min OI change is bigger than Max OI change');
+					return false;					
+				} else {
+					return true;
+				}
+			} else if(valueCount > 0) {
+				return true;
+			} else {
+				toastr.error('Lack OI Change range parameters!');
+				return false;
+			}
+		}
+		
+		ctrl.drawOIChart = function(oiList) {
+			option = oiList[0].stockSymbol + ' ' + oiList[0].strike + ' ' + oiList[0].callPut + ' 20' + oiList[0].expiration;
 			
 			Highcharts.chart('oichart', {
 				chart : {
 					zoomType : 'x'
 				},
 				title: {                                                                
-					text: '<b>'+ option + '</b>' + ' OI Change'
+					text: '<b>'+ option + '</b>' + ' ---- OI Chart'
 				},                                                                      
 				xAxis: {
 					type : 'datetime',
@@ -81,30 +175,73 @@ angular.module(window.tsc.constants.DASHBOARD_APP).component('oiChart', {
 					formatter: function() {  
 						return formatDate(this.x) +' : ' + '<b>'+ this.y + '</b>';
 					}
-				},
-				legend: {
-					enabled: false
-				},                                                                                                                                       
+				},                                                                                                                                   
 				series: [{
 					name: 'Open Interest',
 					data: (function() {
 						var data = []
 						for(var i = 0; i < oiList.length; i++) {
+							// add event day for satisfied Liu
+							x_date = new Date(oiList[i].eventDay / 10000 + 2000, oiList[i].eventDay % 10000 / 100 - 1, oiList[i].eventDay % 100);
+							x_date.setDate(x_date.getDate() + 1);
 							data.push({
-								x :  new Date(oiList[i].eventDay / 10000 + 2000, oiList[i].eventDay % 10000 / 100 - 1, oiList[i].eventDay % 100),
+								x : x_date,
 								y : oiList[i].openInterest
 							});
 						}
-						console.log(data);
 						return data;
 					})()
 				}]
 			});                
 			
-		}
+		};
 		
+		ctrl.drawOIChangeChart = function(oiList) {
+			option = oiList[0].stockSymbol + ' ' + oiList[0].strike + ' ' + oiList[0].callPut + ' 20' + oiList[0].expiration;
+			
+			Highcharts.chart('oiChangeChart', {
+				chart : {
+					zoomType : 'x'
+				},
+				title: {                                                                
+					text: '<b>'+ option + '</b>' + ' Open Interest Change'
+				},                                                                      
+				xAxis: {
+					type : 'datetime',
+					tickmarkPlacement: 'between'
+				},                                                                      
+				yAxis: {
+					title: {                                                            
+						text: 'Open Interest Change'                                                   
+					}                                                            
+				},                                                                      
+				tooltip: {                                                              
+					formatter: function() {  
+						return formatDate(this.x) +' : ' + '<b>'+ this.y + '</b>';
+					}
+				},
+				legend: {
+					enabled: false
+				},                                                                                                                                       
+				series: [{
+					name: 'Open Interest Change',
+					data: (function() {
+						var data = []
+						for(var i = 1; i < oiList.length; i++) {
+							x_date = new Date(oiList[i].eventDay / 10000 + 2000, oiList[i].eventDay % 10000 / 100 - 1, oiList[i].eventDay % 100);
+							data.push({
+								x : x_date,
+								y : oiList[i].openInterest - oiList[i - 1].openInterest
+							});
+						}
+						return data;
+					})()
+				}]
+			});                
+			
+		};		
 // ------------------------- General Functions --------------------------------------------
-		function getFormatData(formData) {
+		function getFormatOptionData(formData) {
 			result = {};
 			for(var col in formData) {
 				result[col] = formData[col];
