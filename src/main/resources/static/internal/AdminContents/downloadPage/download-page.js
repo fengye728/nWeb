cfe98d46cf4ad43a943ced468cfb702cf82f0b57
Peter <UNKNOWN>
2018-08-25 19:47:59 +0800

    Change the server url and port and the way changing in Download page

--- src/main/resources/static/internal/AdminContents/downloadPage/download-page.js
+++ src/main/resources/static/internal/AdminContents/downloadPage/download-page.js
@@ -5,5 +5,11 @@ angular.module(window.tsc.constants.DASHBOARD_APP).component('downloadPage', {
     templateUrl : '/internal/AdminContents/downloadPage/download-page.html',
     controller : function($http, NgTableParams, $q, toastr) {
 		var ctrl = this;
+		//ctrl.server = '52.205.81.17'
+		ctrl.server = 'localhost';
+		ctrl.serverPort = 8080
+		
+		ctrl.url = ctrl.server + ":" + ctrl.serverPort
+		
 	}
 });
